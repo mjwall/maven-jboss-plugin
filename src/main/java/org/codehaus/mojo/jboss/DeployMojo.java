36f35db3f28338e901182286879ca543ab351d64
jgenender <jgenender@52ab4f32-60fc-0310-b215-8acea882cd1b>
2009-03-31 17:34:25 +0000

    MJBOSS-14 - add support for URL encoding before deployment
    git-svn-id: http://svn.codehaus.org/mojo/trunk/mojo/jboss-maven-plugin@9320 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/main/java/org/codehaus/mojo/jboss/DeployMojo.java
+++ src/main/java/org/codehaus/mojo/jboss/DeployMojo.java
@@ -15,18 +15,19 @@
  */
 package org.codehaus.mojo.jboss;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.Iterator;
 
 import org.apache.maven.plugin.MojoExecutionException;
 
 /**
- *  Deploys a directory or file to JBoss via JMX.
+ * Deploys a directory or file to JBoss via JMX.
  *
  * @author <a href="mailto:jgenender@apache.org">Jeff Genender</a>
  * @goal deploy
  */
 public class DeployMojo extends AbstractDeployerMojo {
-
     /**
      * The deployment URL.
      *
@@ -35,20 +36,39 @@ public class DeployMojo extends AbstractDeployerMojo {
      */
     protected String deployUrlPath;
 
+    /**
+     * The character encoding for the fileName.
+     *
+     * @parameter expression="UTF-8"
+     */
+    protected String fileNameEncoding;
+
     public void execute() throws MojoExecutionException {
 
         //Fix the ejb packaging to a jar
 
-        if (fileNames == null) return;
+        if (fileNames == null) {
+            return;
+        }
         Iterator iter = fileNames.iterator();
         while (iter.hasNext()) {
 
-            String fileName = (String)iter.next();
+            String fileName = (String) iter.next();
             String fixedFile = null;
-            if (fileName.toLowerCase().endsWith("ejb")){
+            if (fileName.toLowerCase().endsWith("ejb")) {
                 fixedFile = fileName.substring(0, fileName.length() - 3) + "jar";
             } else {
                 fixedFile = fileName;
+            }
+
+            try {
+                String encoding = fileNameEncoding;
+                if (encoding == null) {
+                    encoding = "UTF-8";
+                }
+                fixedFile = URLEncoder.encode(fixedFile, encoding);
+            } catch (UnsupportedEncodingException ex) {
+                throw new MojoExecutionException(ex.getMessage());
             }
 
             getLog().info("Deploying " + fixedFile + " to JBoss.");
